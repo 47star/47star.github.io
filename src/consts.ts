3e7bd3435b0345700a8ef76bc21b3d7814d0c6bd
DongHoon Yoo <yoo@donghoon.net>
2025-04-12 00:38:07 +0900

    chore: Update site description

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,6 @@
 export const SITE_TITLE = "Lumière";
 export const SITE_DOMAIN = "blog.donghoon.net";
-export const SITE_DESCRIPTION =
-  "A journey of exploration toward the ultimate in systems.";
+export const SITE_DESCRIPTION = "Journey to the ultimate system";
 
 export const AUTHOR_NAME = "DongHoon Yoo";
 export const AUTHOR_EMAIL = "fret49watches@icloud.com";
