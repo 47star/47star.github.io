b66b30480af75702c4245118b9d7776fe3371653
DongHoon Yoo <yoo@donghoon.net>
2025-04-15 10:59:05 +0900

    feat: Add compile class transformer

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,24 @@ import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
+import UnoCSS from "unocss/astro";
+import transformerCompileClass from "@unocss/transformer-compile-class";
 import { SITE_DOMAIN } from "./src/consts";
 
 export default defineConfig({
   site: `https://${SITE_DOMAIN}`,
 
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    UnoCSS({
+      transformers: [
+        transformerCompileClass({
+          classPrefix: "rvl-",
+        }),
+      ],
+    }),
+  ],
 
   vite: {
     plugins: [tailwindcss()],
