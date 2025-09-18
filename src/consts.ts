288098499467077d47204d2a46b4686575e3a863
DongHoon Yoo <yoo@donghoon.net>
2025-09-18 13:30:38 +0900

    chore: Declare the opengraph image url in the constants

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,8 @@
 export const SITE_TITLE = "DongHoon Yoo";
 export const SITE_DOMAIN = "blog.donghoon.net";
 export const SITE_DESCRIPTION = "Journey to the ultimate system";
+export const SITE_OPENGRAPH_IMAGE_URL =
+  "https://repository-images.githubusercontent.com/402295162/5af53d5c-adcf-4a74-8e6c-83fd0c3164b6";
 
 export const AUTHOR_NAME = "DongHoon Yoo";
 export const AUTHOR_EMAIL = "fret49watches@icloud.com";
